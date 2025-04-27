54c6d915209cc32cef04ba059443fcced5d4ab4e
CJW <1427628179@qq.com>
2025-04-27 22:05:30 +0800

    update config

--- Config.py
+++ Config.py
@@ -1,20 +1,21 @@
 Conn = {
-    'connections': {
-        'default': {
-            'engine': 'tortoise.backends.mysql',  # 'tortoise.backends.mysql',
-            'credentials': {
-                'host': '192.168.139.131',
-                'port': '3306',
-                'user': 'root',
-                'password': 'root',
-                'database': 'book',
-                'minsize': 1,
-                'maxsize': 5,
-                'charset': 'utf8mb4',
-                "echo": True
-            }
-        }
-    },
+    "connections": {"default": "mysql://root:root@192.168.139.131:3306/book"},
+    # 'connections': {
+    #     'default': {
+    #         'engine': 'tortoise.backends.mysql',  # 'tortoise.backends.mysql',
+    #         'credentials': {
+    #             'host': '192.168.139.131',
+    #             'port': '3306',
+    #             'user': 'root',
+    #             'password': 'root',
+    #             'database': 'book',
+    #             'minsize': 1,
+    #             'maxsize': 5,
+    #             'charset': 'utf8mb4',
+    #             "echo": True
+    #         }
+    #     }
+    # },
     'apps': {
         'models': {
             'models': ['models','blueprints.models', 'aerich.models'],
@@ -24,7 +25,6 @@ Conn = {
     'use_tz': False,
     'timezone': 'Asia/Shanghai'
 }
-
 
 def ToJsonList(lists):
     return [models.to_json() for models in lists]
